91b61cd0413c3be102eefded5c0be3bb630bc573
Joe <chibyke910@gmail.com>
2025-11-28 12:26:01 +0100

    connected DB again

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,8 +13,9 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    
-    async session({ session }) {
+
+    callbacks: {
+        async session({ session }) {
         const sessionUser = await User.findOne({
             email: session.user.email
         })
@@ -45,6 +46,7 @@ const handler = NextAuth({
         } catch (error) {
 
         }
+    }
     }
 })
 
